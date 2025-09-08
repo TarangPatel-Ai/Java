d7d071583ffdb6cf44ad80819080b6bb9a55c831
TarangPatel <tarangpatel2025@gmail.com>
2025-09-08 16:18:18 +0530

    update the files

--- FirstDemo/src/com/caltech/logic/BaseLogic.java
+++ FirstDemo/src/com/caltech/logic/BaseLogic.java
@@ -3,5 +3,10 @@ package com.caltech.logic;
 public class BaseLogic {
 	public int a = 15;
 	public int b = 15;
-	public int c = a+b;
+	
+	
+	public void Display() {
+		System.out.println("The sum of A and B is: " +(a+b));
+	}
+	
 }
